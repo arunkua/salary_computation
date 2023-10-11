fce671120e5ae249bdeb3a9d612a2bbb7363ecf4
arunkua <arunsingla000@gmail.com>
2023-10-11 13:09:04 +0530

    first_commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask_restful import Resource,Api
+from flask import Flask,jsonify, make_response, request
+import pandas as pd
+
+app=Flask(__name__)
+api=Api(app)
+
+df=pd.read_csv("s3://talentprofile/arun_ml_work/salary_testing/modified_data.csv",
+          storage_options={'key':'AKIASXCE4245H5IMJIOM',
+                'secret':'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'})
+
+
+class InternalSalaryAPI(Resource):
+    def get(self):
+        query = request.args.get("query")
+        res=dict()
+        try:
+            res['Average_Salary']=df[df['input_job_title']==query]['Average_salary'].values[0]
+            res['title']=query
+            status_code=200
+            result = {"status" : "success",
+                                        "salary" : res}
+
+        except:
+            status_code=400
+            result = {"status" : "error",
+                                        "salary" :""}
+        return make_response(jsonify(result), status_code)
+
+api.add_resource(InternalSalaryAPI,'/salary_testing')
+
+if __name__=='__main__':
+    app.run()
